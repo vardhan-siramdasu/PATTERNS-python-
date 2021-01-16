2f49cec201c8a45a839e3e95a6129376ab897f2b
vardhan <54410907+vardhan-siramdasu@users.noreply.github.com>
2021-01-16 19:09:31 +0530

    Add files via upload

---
+++ X_mass tree/X_mass tree.py
@@ -0,0 +1,11 @@
+for i in range(1,5):
+    print(' '*(4-i+5)+'* '*i)
+
+for i in range(3,7):
+    print(' '*(7-i+2)+'* '*i)
+
+for i in range(5,9):
+    print(' '*(9-i)+'* '*i)
+    
+for i in range(4):
+    print(' '*5+'* '*4)
