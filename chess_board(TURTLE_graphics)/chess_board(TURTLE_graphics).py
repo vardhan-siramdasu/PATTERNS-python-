a98272edd9fdf11afd84eb8fbc1e7a3cf22aa481
vardhan <54410907+vardhan-siramdasu@users.noreply.github.com>
2021-01-16 12:38:39 +0530

    Add files via upload

---
+++ chess_board(TURTLE_graphics)/chess_board(TURTLE_graphics).py
@@ -0,0 +1,17 @@
+import turtle as t
+t.speed(0)
+t.color('black')
+for i in range(8):
+    for j in range(8):
+        if((i+j)%2==0):
+            t.begin_fill()
+        for k in range(4):
+            t.fd(40)
+            t.lt(90)
+        t.end_fill()
+        t.fd(40)
+    t.pu()
+    t.goto(0,-(i+1)*40) 
+    t.pd()
+t.done()
+t.exitonclick()
