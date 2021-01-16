6868f0c857be2463afa68242cb8be857082510fb
vardhan <54410907+vardhan-siramdasu@users.noreply.github.com>
2021-01-16 16:12:15 +0530

    Update Pascal's_triangle.py

--- pascal's_triangle/Pascal's_triangle.py
+++ pascal's_triangle/Pascal's_triangle.py
@@ -1,22 +1,22 @@
 arr=[]
 print('enter noof rows')
 rows=int(input())
-if rows%2==0:
-    even_or_odd=0
+if rows % 2 == 0:
+    even_or_odd = 0
 else:
-    even_or_odd=1
+    even_or_odd = 1
 for i in range(rows):
     arr.append([])
-    for j in range(i+rows):
-        if ((i+j <rows-1)or((i+j)%2==even_or_odd)):
+    for j in range(i + rows):
+        if ((i + j < rows - 1) or ((i + j)%2 == even_or_odd)):
             arr[i].append('  ')
-        elif(i+j==rows-1 or len(arr[i])==rows-1+i):
+        elif(i + j == rows - 1 or len(arr[i]) == rows - 1 + i):
             arr[i].append(1)
         else:
-            value=arr[i-1][j-1]+arr[i-1][j+1]
+            value=arr[i - 1][j - 1] + arr[i - 1][j + 1]
             arr[i].append(value)
 print('Pascals triangle :')
 for i in arr:
     for j in i:
-        print(j,end='    ')
+        print(j ,end = '    ')
     print()
